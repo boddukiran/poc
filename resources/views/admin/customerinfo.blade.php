065570b5114e1639b069bc85130562ca5b3908d5
Swetha M <swetha.merige@caresync.com>
2018-07-02 16:08:32 +0530

    extra spaces has been removed

--- resources/views/admin/customerinfo.blade.php
+++ resources/views/admin/customerinfo.blade.php
@@ -16,52 +16,52 @@
 
 @section('content')
 <div class="container login-content">
-  <div class="row justify-content-center">
-    <div class="col-4">
+    <div class="row justify-content-center">
+        <div class="col-4">
             <h1>Customer Details</h1>
             <form id="register-form" method="post" action="{{action('AdminController@updateCustomerInfo')}}">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <input type="hidden" name="cid" id="cid" value="{{ $customerInfo->id }}">
-             <div class="form-group">                
-                <input type="text" name="first_name" class="form-control" placeholder="First Name" value="{{$customerInfo->fname}}">
-            </div>
-            <div class="form-group">                
-                <input type="text" name="last_name" class="form-control" placeholder="Last Name" value="{{$customerInfo->lname}}">                
-            </div>            
-            <div class="form-group">               
-                <input type="email" class="form-control" placeholder="Email" name="email" value="{{$customerInfo->email}}">                
-            </div>         
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="gender" value="1" <?=($customerInfo->gender == 1) ?'checked="checked"':''?>>
-                <label class="form-check-label" style="width:70px;margin-bottom:15px;">
-                    Male
-                </label>
-                <input class="form-check-input" type="radio" name="gender" value="0" <?=($customerInfo->gender == 0)? 'checked="checked"':''?>>
-                <label class="form-check-label" >
-                    Female
-                </label>
-            </div>             
-            <div class="form-group">                
-                <input type="text" name="country" class="form-control" placeholder="Country" value="{{$customerInfo->country}}">                
-            </div>
-            <div class="form-group">                
-                <input type="text" name="state" class="form-control" placeholder="State" value="{{$customerInfo->state}}">                
-            </div>
-            <div class="form-group">                
-                <input type="text" name="city" class="form-control" placeholder="City" value="{{$customerInfo->city}}">                
-            </div>
-            <div class="form-group">                
-                <input type="text" name="zipcode" class="form-control" placeholder="zipcode" value="{{$customerInfo->countrycode}}">                
-            </div> 
-            <div class="form-group">                
-                <input type="text" name="address" class="form-control" placeholder="Address" value="{{$customerInfo->address}}">                
-            </div>
-            <div class="form-group">                
-                <input type="text" name="mobile" class="form-control" placeholder="mobile" value="{{$customerInfo->mobile}}">                
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+                <div class="form-group">
+                    <input type="text" name="first_name" class="form-control" placeholder="First Name" value="{{$customerInfo->fname}}">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="last_name" class="form-control" placeholder="Last Name" value="{{$customerInfo->lname}}">
+                </div>
+                <div class="form-group">
+                    <input type="email" class="form-control" placeholder="Email" name="email" value="{{$customerInfo->email}}">
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="gender" value="1" <?= ($customerInfo->gender == 1) ? 'checked="checked"' : '' ?>>
+                    <label class="form-check-label" style="width:70px;margin-bottom:15px;">
+                        Male
+                    </label>
+                    <input class="form-check-input" type="radio" name="gender" value="0" <?= ($customerInfo->gender == 0) ? 'checked="checked"' : '' ?>>
+                    <label class="form-check-label" >
+                        Female
+                    </label>
+                </div>
+                <div class="form-group">
+                    <input type="text" name="country" class="form-control" placeholder="Country" value="{{$customerInfo->country}}">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="state" class="form-control" placeholder="State" value="{{$customerInfo->state}}">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="city" class="form-control" placeholder="City" value="{{$customerInfo->city}}">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="zipcode" class="form-control" placeholder="zipcode" value="{{$customerInfo->countrycode}}">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="address" class="form-control" placeholder="Address" value="{{$customerInfo->address}}">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="mobile" class="form-control" placeholder="mobile" value="{{$customerInfo->mobile}}">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
+        </div>
     </div>
-  </div>
 </div>
- @endsection
+@endsection
--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -16,26 +16,26 @@
 
 @section('content')
 <div class="container login-content">
-  <div class="row justify-content-center">
-    <div class="col-4">
+    <div class="row justify-content-center">
+        <div class="col-4">
             <h1> Messages </h1>
             <form id="login-form" method="POST" action="{{ action('CustomerController@saveMessage') }}">
-            {{ csrf_field() }}
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" name="email" placeholder="Enter email">
-            </div>
-            <div class="form-group">
-                <label for="exampleInputSubject1">Subject</label>
-                <input type="text" class="form-control" name="subject" placeholder="Subject">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlTextarea1">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" name="message" rows="3"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" name="email" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputSubject1">Subject</label>
+                    <input type="text" class="form-control" name="subject" placeholder="Subject">
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" name="message" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
+        </div>
     </div>
-  </div>
 </div>
- @endsection
+@endsection
